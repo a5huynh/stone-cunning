a9f4f54a9436128c84c1989e4b845f8c22aaccf8
Andrew Huynh <a5thuynh@gmail.com>
2019-09-24 23:36:54 -0700

    Updating libpath example

--- libpath/examples/main.rs
+++ libpath/examples/main.rs
@@ -1,23 +1,30 @@
-use libpath::{dijkstra};
+use libpath::dijkstra;
 use libterrain::{Biome, Point3, TerrainChunk};
 
 const TEST_WIDTH: u32 = 30;
 const TEST_HEIGHT: u32 = 15;
-const TEST_DEPTH: u32 = 2;
+const TEST_DEPTH: u32 = 3;
+
+pub fn biome_to_ascii(biome: Option<Biome>) -> char {
+    match biome {
+        Some(Biome::ROCK) => '\'',
+        Some(Biome::GRASSLAND) => ',',
+        _ => '.',
+    }
+}
 
 pub fn init_terrain(terrain: &mut TerrainChunk) {
-    terrain.set((21, 0, 0), Some(Biome::ROCK));
-    terrain.set((21, 1, 0), Some(Biome::ROCK));
-    terrain.set((21, 2, 0), Some(Biome::ROCK));
-    terrain.set((21, 3, 0), Some(Biome::ROCK));
-    terrain.set((21, 4, 0), Some(Biome::ROCK));
-    terrain.set((21, 5, 0), Some(Biome::ROCK));
-    terrain.set((22, 5, 0), Some(Biome::ROCK));
-    terrain.set((23, 5, 0), Some(Biome::ROCK));
-    terrain.set((24, 5, 0), Some(Biome::ROCK));
-    terrain.set((25, 5, 0), Some(Biome::ROCK));
-    terrain.set((26, 5, 0), Some(Biome::ROCK));
-    terrain.set((27, 5, 0), Some(Biome::ROCK));
+    // Fill the bottom level w/ rock
+    for y in 0..TEST_HEIGHT {
+        for x in 0..TEST_WIDTH {
+            terrain.set((x, y, 0), Some(Biome::ROCK));
+        }
+    }
+
+    // Add a 1 block high wall in the 2nd level.
+    for y in 0..TEST_HEIGHT {
+        terrain.set((15, y, 1), Some(Biome::ROCK));
+    }
 }
 
 pub fn main() {
@@ -25,7 +32,7 @@ pub fn main() {
     let mut terrain = TerrainChunk::new(TEST_WIDTH, TEST_HEIGHT);
     init_terrain(&mut terrain);
 
-    let (parents, path) = dijkstra(&terrain, Point3::new(8, 7, 0), Point3::new(17, 2, 0));
+    let (parents, path) = dijkstra(&terrain, Point3::new(8, 7, 1), Point3::new(17, 2, 1));
     // Draw parents
     for z in (0..TEST_DEPTH).rev() {
         println!("z-level: {}", z);
@@ -33,18 +40,14 @@ pub fn main() {
         for y in 0..TEST_HEIGHT {
             for x in 0..TEST_WIDTH {
                 let pt = Point3::new(x, y, z);
-                if !terrain.is_passable(&pt) {
-                    print!("#");
+                if path.contains(&pt) {
+                    print!("@");
                 } else {
-                    if path.contains(&pt) {
-                        print!("@");
+                    if parents.contains_key(&pt) {
+                        let came_from = parents[&pt];
+                        print!("{}", direction((x, y, z), &came_from));
                     } else {
-                        if parents.contains_key(&pt) {
-                            let came_from = parents[&pt];
-                            print!("{}", direction((x, y), &came_from));
-                        } else {
-                            print!(".");
-                        }
+                        print!("{}", biome_to_ascii(terrain.get(pt.x, pt.y, pt.z)));
                     }
                 }
                 print!(" ");
@@ -55,11 +58,12 @@ pub fn main() {
     }
 }
 
-pub fn direction(pt: (u32, u32), start: &Point3<u32>) -> String {
+pub fn direction(pt: (u32, u32, u32), start: &Point3<u32>) -> String {
     let xdir = pt.0 as i32 - start.x as i32;
     let ydir = pt.1 as i32 - start.y as i32;
+    let zdir = pt.2 as i32 - start.z as i32;
 
-    if ydir == 0 && xdir == 0 {
+    if ydir == 0 && xdir == 0 && zdir == 0 {
         return String::from("A");
     }
 
@@ -73,6 +77,11 @@ pub fn direction(pt: (u32, u32), start: &Point3<u32>) -> String {
     // Left /right
     if ydir > 0 {
         return String::from("^");
+    }
+
+    // zlevels
+    if zdir > 0 || zdir < 0 {
+        return String::from("X");
     }
 
     return String::from("V");
