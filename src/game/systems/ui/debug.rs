fbbf49adfd9e9f9d2e30b771f855cf6c17a6fb75
Andrew Huynh <a5thuynh@gmail.com>
2019-10-06 14:06:57 -0700

    Run debug info about each worker

--- src/game/systems/ui/debug.rs
+++ src/game/systems/ui/debug.rs
@@ -1,6 +1,4 @@
-use amethyst::{
-    ecs::{Entities, ReadExpect, ReadStorage, System, Write},
-};
+use amethyst::ecs::{Entities, Join, ReadExpect, ReadStorage, System, Write};
 use amethyst_imgui::imgui::{im_str, Condition, Window};
 
 use libdwarf::{
@@ -13,7 +11,10 @@ use libdwarf::{
 use crate::game::components::CursorSelected;
 
 #[derive(Default)]
-pub struct DebugUI;
+pub struct DebugUI {
+    new_worker_pos: [i32; 3],
+}
+
 impl<'s> System<'s> for DebugUI {
     type SystemData = (
         Entities<'s>,
@@ -23,34 +24,32 @@ impl<'s> System<'s> for DebugUI {
         Write<'s, TaskQueue>,
     );
 
-    fn run(
-        &mut self,
-        (
-            entities,
-            objects,
-            workers,
-            cursor_selected,
-            mut queue,
-        ): Self::SystemData,
-    ) {
+    fn run(&mut self, (entities, objects, workers, cursor_selected, mut queue): Self::SystemData) {
         amethyst_imgui::with(|ui| {
             Window::new(im_str!("Workers"))
                 .size([300.0, 100.0], Condition::FirstUseEver)
                 .build(ui, || {
+                    ui.input_int3(im_str!("map pos"), &mut self.new_worker_pos)
+                        .build();
+
                     if ui.button(im_str!("Add Worker"), [0.0, 0.0]) {
-                        queue.add_world(Action::AddWorker(Point3::new(1, 1, 0)));
+                        queue.add_world(Action::AddWorker(Point3::new(
+                            self.new_worker_pos[0] as u32,
+                            self.new_worker_pos[1] as u32,
+                            self.new_worker_pos[2] as u32,
+                        )));
                     }
 
-                    if ui.button(im_str!("Add Resource"), [0.0, 0.0]) {
-                        queue.add_world(Action::Add(Point3::new(9, 9, 0), String::from("tree")));
-                    }
+                    ui.separator();
 
-                    if ui.button(im_str!("Add Task"), [0.0, 0.0]) {
-                        queue.add(Action::HarvestResource(
-                            Point3::new(9, 9, 0),
-                            String::from("tree"),
-                            String::from("wood"),
-                        ));
+                    for (entity, worker) in (&entities, &workers).join() {
+                        if ui
+                            .collapsing_header(&im_str!("Worker {}", entity.id()))
+                            .build()
+                        {
+                            ui.text(&im_str!("action queue: {}", worker.actions.len()));
+                            ui.text(&im_str!("inventory: {}", worker.inventory.len()));
+                        }
                     }
                 });
 
@@ -59,38 +58,50 @@ impl<'s> System<'s> for DebugUI {
                 .build(ui, || {
                     let selected = &cursor_selected.hover_selected;
                     if let Some(pick_info) = selected {
-                        let worker_label = pick_info.worker
+                        let worker_label = pick_info
+                            .worker
                             .and_then(|worker_id| {
                                 let entity = entities.entity(worker_id);
                                 workers.get(entity)
                             })
-                            .and_then(|worker| Some(format!("worker: {}", worker.to_string())))
-                            .unwrap_or("worker: N/A".to_string());
+                            .and_then(|worker| Some(format!("Worker: {}", worker.to_string())))
+                            .unwrap_or("Worker: N/A".to_string());
                         ui.text(worker_label);
 
-                        let object_label = pick_info.object
+                        let object_label = pick_info
+                            .object
                             .and_then(|object_id| {
                                 let entity = entities.entity(object_id);
                                 objects.get(entity)
                             })
-                            .and_then(|object| Some(format!("object: {}", object.to_string())))
-                            .unwrap_or("object: N/A".to_string());
+                            .and_then(|object| Some(format!("Object: {}", object.to_string())))
+                            .unwrap_or("Object: N/A".to_string());
                         ui.text(object_label);
 
-                        let terrain_label = pick_info.terrain.as_ref()
-                            .and_then(|terrain| Some(format!("terrain: {:?}", terrain)))
-                            .unwrap_or("terrain: N/A".to_string());
+                        let terrain_label = pick_info
+                            .terrain
+                            .as_ref()
+                            .and_then(|terrain| Some(format!("Terrain: {:?}", terrain)))
+                            .unwrap_or("Terrain: N/A".to_string());
                         ui.text(terrain_label);
 
-                        let map_pos = pick_info.position
+                        let map_pos = pick_info
+                            .position
                             .and_then(|position| {
-                                Some(format!("pos: ({}, {}, {})", position.x, position.y, position.z))
+                                Some(format!(
+                                    "Map Pos: ({}, {}, {})",
+                                    position.x, position.y, position.z
+                                ))
                             })
-                            .unwrap_or("pos: N/A".to_string());
+                            .unwrap_or("Map Pos: N/A".to_string());
                         ui.text(map_pos);
 
                         let mouse_pos = ui.io().mouse_pos;
-                        ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
+                        ui.text(im_str!(
+                            "Mouse Pos: ({:.1},{:.1})",
+                            mouse_pos[0],
+                            mouse_pos[1]
+                        ));
                     }
                 });
         });
